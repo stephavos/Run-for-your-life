80a485616976ca9f6dba22bad9886b0baa9ae12f
stephavos <39690218+stephavos@users.noreply.github.com>
2018-11-15 20:00:02 +0100

    Controller Modification

--- controller.java
+++ controller.java
@@ -1,51 +1,61 @@
-package controller;
+package Controller;
 
 import java.awt.Color;
-
-import model.ILevel;
+import java.util.Scanner;
+import model.ILvl;
 import model.ITile;
-import model.ITimer;
 import view.IView;
 
 public class Controller implements IController{
 	
-	public ILevel level;
+	public ILvl lvl;
 	public IView view;
-	
 	int countgrey;
-	
-	public  Controller(ILevel level, IView view) {
+	private Scanner sc;
+
+	public  Controller(ILvl lvl, IView view) {
 		this.view= view;
-		this.level=level;
+		this.lvl=lvl;
 	}
-		
+	
 	public void play() {
-		level.getTimer().start();
+		lvl.getTimer().start();
 		System.out.println("Play");
-		while(level.getPlayer().isAlive() && countgrey!=1){
+		while(lvl.getrunner().isAlive() && countgrey!=1){
 		
 		countgrey=0;	
-		for (ITile tile : level.getMap()) {
+		for (ITile tile : lvl.getMap()) {
 			tile.reduce();
 			countgrey +=(tile.getColor()==Color.GRAY)?1:0;
-			if(tile.getX()==level.getPlayer().getX()&& tile.getY()==level.getPlayer().getY() && tile.getColor()==Color.BLACK) {level.getPlayer().setAlive(false);}
-			
-	
+			if(tile.getX()==lvl.getrunner().getX()&& tile.getY()==lvl.getrunner().getY() && tile.getColor()==Color.BLACK) {
+				lvl.getrunner().setAlive(false);
+			}
 		}
-				
 		try {
 			Thread.sleep(10);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
+		} 
+		catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+	}
+		
+		lvl.getTimer().stop();
+
+		
+		if(countgrey==1){
+
+			sc = new Scanner(System.in);
+			System.out.println("Veuillez saisir votre nom :");
+			String scr = sc.nextLine();
+			lvl.save(scr);
+			this.view.displayMessage("Vous avez gagné! Bravo vous avez réussi le jeu en : " + lvl.getTimer().getTime());
+			System.out.println("Saved");
 		}
 		
-		level.getTimer().stop();
-		if(countgrey==1)this.view.displayMessage("You win" + level.getTimer().getTime());
-			
-		else{this.view.displayMessage("Game Over in" + level.getTimer().getTime());}
-		
-	}
+		else{
+			System.out.println("Veuillez saisir votre nom :");
+			this.view.displayMessage("Vous avez perdu! Désoler vous avez survécu pendant : " + lvl.getTimer().getTime());
 
+		}
+	}
 }
